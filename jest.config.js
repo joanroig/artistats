80793500dd6fd94271807364e9280ba7daf03762
Joan Roig <12451419+joanroig@users.noreply.github.com>
2022-08-25 18:29:59 +0200

    feat: added ngx-rocket template

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+require('jest-preset-angular/ngcc-jest-processor');
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+const { compilerOptions } = require('./tsconfig.json');
+
+module.exports = {
+  preset: 'jest-preset-angular',
+  roots: ['src'],
+  coverageDirectory: 'reports',
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  globals: {
+    'ts-jest': {
+      allowSyntheticDefaultImports: true,
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      diagnostics: {
+        ignoreCodes: ['TS151001'],
+      },
+    },
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+};
