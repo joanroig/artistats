7867164b8fe3d731807501f19494dbfc1c6d3376
Joan Roig <joanroigsegura@gmail.com>
2022-08-25 18:29:59 +0200

    feat: added ngx-rocket template

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { Angulartics2Module } from 'angulartics2';
+
+import { SharedModule } from '@shared';
+import { MaterialModule } from '@app/material.module';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    TranslateModule,
+    SharedModule,
+    FlexLayoutModule,
+    MaterialModule,
+    Angulartics2Module,
+    HomeRoutingModule,
+  ],
+  declarations: [HomeComponent],
+})
+export class HomeModule {}
