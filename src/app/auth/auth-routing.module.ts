cf535fb0475d06dde62bcc8a9e1e5e0c335660f3
Joan Roig <12451419+joanroig@users.noreply.github.com>
2022-11-26 00:03:26 +0100

    feat: move all pages into a folder

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 
-import { LoginComponent } from './login.component';
+import { LoginComponent } from '@app/pages/login/login.component';
 
 const routes: Routes = [{ path: 'login', component: LoginComponent, data: { title: marker('Login') } }];
 
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { SharedModule } from '@shared';
-import { MaterialModule } from '@app/material.module';
 import { I18nModule } from '@app/i18n';
+import { MaterialModule } from '@app/material.module';
+import { LoginComponent } from '@app/pages/login/login.component';
+import { SharedModule } from '@shared';
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './login.component';
 
 @NgModule({
   imports: [
